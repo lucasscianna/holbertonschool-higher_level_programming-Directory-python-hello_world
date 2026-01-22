6e23ac96f1e2f1371de2a4174d3b950586357df4
lucasscianna <sciannalucas0@gmail.com>
2026-01-22 14:11:15 +0000

    .

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-    return None
-best_key = None
-best_value = float("-inf")
-for key
+        return None
+
+    best_key = None
+    best_value = float("-inf")
+
+    for key in a_dictionary:
+        if a_dictionary[key] > best_value:
+            best_value = a_dictionary[key]
+            best_key = key
+
+    return best_key
+    
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     unique_list = []
     for number in my_list:
         if number not in unique_list:
-           unique_list.append(number)
+         unique_list.append(number)
     total = 0
     for number in unique_list:
         total += number
