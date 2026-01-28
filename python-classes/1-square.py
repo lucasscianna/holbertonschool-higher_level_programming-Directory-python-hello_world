575ac5f2a514f6b2183e5dc15f0a3709df1b666b
lucasscianna <sciannalucas0@gmail.com>
2026-01-28 09:01:07 +0000

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
 class Square:
+    """Class that defines a square by its size."""
+
     def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square.
+        """
         self.__size = size
-        
